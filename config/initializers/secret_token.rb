74a53c9b12ea2e582aabeae5479d21fe44346785
Seliverstov Maxim <maxim.web.developer@gmail.com>
2013-09-28 14:52:58 +0400

    Initialize app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hyip::Application.config.secret_key_base = '13b72f430b4fc308404d1e28958a1a6ced6f98cd832eb04682979ad343ad9fcb89c9292775f40463d3981a4264bb1df0884950e4ad80e39d6eb4638ac4c6c3fe'
